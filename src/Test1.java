ba29e4afc7e47d5256bc506d074ea046cfd813fd
Gulay <gulaykurt124@gmail.com>
2024-03-23 17:58:37 +0300

    Test1 and Test2 updated

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,10 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Homework1");
         System.out.println("Homework11");
+        System.out.println("Homework111");
+        System.out.println("Homework1111");
+
+
+
     }
 }
--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("HOMEWORK2");
+        System.out.println("HOMEWORK22");
     }
 }
